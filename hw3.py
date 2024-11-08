26f08b439cce986d3f871dbdaf2bd77e24d7a7ca
annalisebuonya <abuonya@calpoly.edu>
2024-11-08 13:09:55 -0800

    update to task 1

--- hw3.py
+++ hw3.py
@@ -10,10 +10,13 @@ from data import CountyDemographics
 def population_total(list:list[CountyDemographics] ) -> int:
     sum_of_2014_population = 0   # create variable to store sum of '2014 Population' values as function iterates through the list.
 
-    for idx in range(1, len(list.population)):
-            temp = idx.population['2014 Population']
+    for idx in list:
+            anothertemp = list[idx]
+            temp = anothertemp.population['2014 Population']
             sum_of_2014_population += temp
-            print(sum_of_2014_population)
-            return sum_of_2014_population
+
+    print (sum_of_2014_population)
+    return sum_of_2014_population
+
 
 
